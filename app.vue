351d510f4fd365129c6a923a3d9911a8b6353c22
kipik <kipik.kipik@gmail.com>
2025-03-19 13:06:29 +0100

    amélioration hero section

--- app.vue
+++ app.vue
@@ -63,18 +63,17 @@
       <div class="absolute inset-0 bg-black opacity-50" />
       <div class="container mx-auto px-4 relative z-10 text-center">
         <h1 class="text-5xl font-bold text-white mb-4">Bienvenue sur Cwowd</h1>
-        <p class="text-xl text-gray-300 mb-8">Découvrez l'actualité ludique et participative</p>
+        <p class="text-xl text-gray-300 mb-8">L'actualité ludique et participative</p>
         <div class="flex items-center justify-center space-x-4">
           <a href="#" class="btn btn-soft btn-primary px-6 py-3  font-semibold hover:bg-primary">Lire
             les
             articles</a>
           <a href="#"
-            class="btn btn-active btn-primary border border-gray-300 text-white px-6 py-3 rounded-md font-semibold hover:bg-gray-700">Rejoindre
+            class="btn btn-active btn-primary border border-gray-300 text-white px-6 py-3 rounded-md font-semibold hover:bg-gray-7">Rejoindre
             le forum</a>
         </div>
-        <p class="text-md text-gray-300 mb-8">Une communauté de joueurs unique avec qui partager votre passion, vos
+        <p class=" text-md text-gray-300 mb-8">Une communauté de joueurs unique avec qui partager votre passion, vos
           découvertes, tests, impressions et bons comme mauvais moments.</p>
-
       </div>
     </section>
 
