c77a1dd46ef7970bb27d9d9003c7d89665caaba9
kipik <kipik.kipik@gmail.com>
2025-03-19 12:51:05 +0100

    ajout shadcdn au nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindcss from '@tailwindcss/vite';
-
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
@@ -17,5 +15,17 @@ export default defineNuxtConfig({
 
   tailwindcss: {
     // Options
+  },
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui',
   },
 });
